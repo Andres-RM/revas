30a92237b8acaa022dae57a885bde8a6e367ff49
andres <ramireznadres1994@gmail.com>
2021-01-19 23:48:30 -0400

    copyright

--- src/components/rFooter.vue
+++ src/components/rFooter.vue
@@ -4,7 +4,7 @@
       <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/ve/" target="_blank" class="lic-img">
         <img src="~assets/lic.png">
       </a>
-      <p class="copy">2021 | <a href="http://www.ucv.ve/" target="_blank">Universidad Central de Venezuela</a>
+      <p class="copy">© 2020 - {{current_year}} | <a href="http://www.ucv.ve/" target="_blank">Universidad Central de Venezuela</a>
       </p>
     </div>
   </footer>
@@ -15,13 +15,17 @@ export default {
   name: 'rFooter',
   data () {
     return {
-      transparent: false
+      transparent: false,
+      current_year: null
     }
   },
   computed: {
     classes () {
       return (this.transparent === true ? ' footer-transparent' : '')
     }
+  },
+  beforeMount () {
+    this.current_year = new Date().getFullYear()
   },
   watch: {
     $route (to) {
